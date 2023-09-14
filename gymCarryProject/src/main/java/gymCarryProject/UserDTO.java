fe1c1b8a38cefaf1d3f23a8c3927ec93ec0725fc
wanding <weew9900@gmail.com>
2023-09-14 17:30:08 +0900

    [feat] UserDTO 클래스 생성 #9

--- gymCarryProject/src/main/java/gymCarryProject/UserDTO.java
+++ gymCarryProject/src/main/java/gymCarryProject/UserDTO.java
@@ -1,6 +1,106 @@
 package gymCarryProject;
 
 public class UserDTO {
+	private String id;
 	private String name;
+	private String pwd;
 	private String email;
+	private String phone;
+	
+	public UserDTO() {}
+	
+	public UserDTO(String id, String name, String pwd, String email, String phone) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.pwd = pwd;
+		this.email = email;
+		this.phone = phone;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPwd() {
+		return pwd;
+	}
+
+	public void setPwd(String pwd) {
+		this.pwd = pwd;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	@Override
+	public String toString() {
+		return "UserDTO [id=" + id + ", name=" + name + ", pwd=" + pwd + ", email=" + email + ", phone=" + phone + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((email == null) ? 0 : email.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((pwd == null) ? 0 : pwd.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		UserDTO other = (UserDTO) obj;
+		if (email == null) {
+			if (other.email != null)
+				return false;
+		} else if (!email.equals(other.email))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (pwd == null) {
+			if (other.pwd != null)
+				return false;
+		} else if (!pwd.equals(other.pwd))
+			return false;
+		return true;
+	}
+	
+	
+	
+	
 }
