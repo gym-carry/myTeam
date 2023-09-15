f4162faa29321445600a66ffbfa4d6f404e1396d
wanding <weew9900@gmail.com>
2023-09-15 13:21:55 +0900

    [feat] UserDAO insert 기능 구현

--- gymCarryProject/src/main/java/gymCarryProject/UserDAO.java
+++ gymCarryProject/src/main/java/gymCarryProject/UserDAO.java
@@ -1,11 +1,48 @@
 package gymCarryProject;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class UserDAO {
 
-	private String id;
-	private String name;
-	private String pwd;
-	private String email;
-	private String phone;
+	private int result;
 	
+	private Connection con;
+	private ConnectionPool pool;
+	private ResultSet rs;
+	
+	public UserDAO () throws ClassNotFoundException {
+		try {
+		String dbURL = "jdbc:oracle:thin:@localhost:1521:xe";
+	    String dbID =  "hr";
+	    String dbPWD = "hr";
+	    Class.forName("oracle.jdbc.OracleDriver");
+	    
+	    pool = ConnectionPool.getInstance(dbURL, dbID, dbPWD, 3, 4, true, 500);
+	} catch(SQLException e) {
+		e.printStackTrace();
+		}
+	}
+	
+	public int insert (UserDTO dto) throws SQLException {
+		con = pool.getConnection();
+		String sql = "insert into R_USER(id, name, pwd, email, phone)"
+		+ "VALUES(?, ?, ?, ?, ?)";
+		PreparedStatement stmt = con.prepareStatement(sql);
+		
+		System.out.println(sql);
+		stmt.setString(1, dto.getId());
+		stmt.setString(2, dto.getName());
+		stmt.setString(1, dto.getPwd());
+		stmt.setString(1, dto.getEmail());
+		stmt.setString(1, dto.getPhone());
+		
+		result = stmt.executeUpdate();
+		stmt.close();
+		pool.releaseConnection(con);
+		return result;
+		
+	}
 }
