8d8d55a1a4feb38684b0eec5616acf07d3eeea0f
rowoon <dev.rowoon@gmail.com>
2023-09-18 18:44:08 +0900

    [mod] 자바스크립트 파일 분

--- gymCarryProject/src/main/webapp/login.js
+++ gymCarryProject/src/main/webapp/login.js
@@ -1,36 +1,23 @@
-const idInput = document.getElementById("id_input");
-const pwdInput = document.getElementById("pwd_input");
-const logInBtn = document.getElementById("submit_btn");
-
-// getInputValue 함수
-const getIdInputValue = (e) => {
-  const idInputValue = e.target.value;
-  return idInputValue;
-};
-
-// getpwdInputValue 함수
-const getpwdInputValue = (e) => {
-  const pwdInputValue = e.target.value;
-  return pwdInputValue;
-};
-
-// onloginHandler 함수
-const onloginHandler = (e) => {
-  e.preventDefault();
-  if (idInput.value === "") {
-    alert("아이디를 입력해주세요");
-  } else if (pwdInput.value === "") {
-    alert("비밀번호를 입력해주세요");
-  } else {
-    alert("로그인을 진행하시겠습니까?");
+ const idInput = document.getElementById("id_input");
+  const pwdInput = document.getElementById("pwd_input");
+  const logInBtn = document.getElementById("submit_btn");
+  const idError = document.getElementById("id_error");
+  const pwdError = document.getElementById("pwd_error");
+  
+  function onClickLoginSubmit(e){
+    e.preventDefault();
+    let result = true;
+    if(idInput.value == ""){ 
+      idInput.className = 'input_id';
+      idError.innerText = "아이디를 입력해주세요";
+      idInput.focus();
+    }else if(pwdInput.value == ""){
+      pwdInput.className = 'input_pwd';
+      pwdError.innerText = '비밀번호를 입력헤주세요';
+      pwdInput.focus();
+    }else{
+  	  form.submit();
+    }
   }
-};
-
-// 아이디 인풋 이벤트
-idInput.addEventListener("input", getIdInputValue);
-// 비번 인풋 이벤트
-pwdInput.addEventListener("input", getpwdInputValue);
-// 제출버튼 이벤트
-logInBtn.addEventListener("click", onloginHandler);
-
-//
+  
+  form.addEventListener('submit', onClickLoginSubmit);
