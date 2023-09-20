9c757a45f475f3c2b24e19de70b4e1ecabe3b068
rowoon <dev.rowoon@gmail.com>
2023-09-20 14:01:33 +0900

    [feat] 페이지번호 수정중.... #34

--- gymCarryProject/src/main/java/gymCarryProject/board/RBoardDAO.java
+++ gymCarryProject/src/main/java/gymCarryProject/board/RBoardDAO.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 import gymCarryProject.ConnectionPool;
 
@@ -25,19 +26,36 @@ public class RBoardDAO {
 			e.printStackTrace();
 		}
 	}
+	
+	public int getNext() throws SQLException {
+		con = pool.getConnection();
+		String sql = "SELECT BOARD_NO FROM R_BOARD ORDER BY BOARD_NO DESC";
+		try {
+			PreparedStatement pstmt = con.prepareStatement(sql);
+			rs = pstmt.executeQuery();
+			if(rs.next()) {
+				return rs.getInt(1) + 1;
+			}
+		}catch(Exception e){
+				e.printStackTrace(); 
+			}
+			
+		return -1;
+	}
 
 	
 	public int write(BoardDTO boardDTO) throws SQLException {
 		con = pool.getConnection();
 		String sql = "insert into R_BOARD(board_no, id, local, company_name, board_title, board_content, board_regdate"
-	            + ", parent, viewcnt)" + "values(R_BOARD_SEQ.NEXTVAL, ?, ?, ? , ? ,? ,sysdate, 0, 0)";
+	            + ", parent, viewcnt)" + "values(?, ?, ?, ? , ? ,? ,sysdate, 0, 0)";
 		try {
 			PreparedStatement pstmt = con.prepareStatement(sql);
-			pstmt.setString(1, boardDTO.getUserId());
-			pstmt.setString(2, boardDTO.getLocal());
-			pstmt.setString(3, boardDTO.getCompanyName());
-			pstmt.setString(4, boardDTO.getBoardTitle());
-			pstmt.setString(5, boardDTO.getBoardContent());	
+			pstmt.setInt(1, getNext());
+			pstmt.setString(2, boardDTO.getUserId());
+			pstmt.setString(3, boardDTO.getLocal());
+			pstmt.setString(4, boardDTO.getCompanyName());
+			pstmt.setString(5, boardDTO.getBoardTitle());
+			pstmt.setString(6, boardDTO.getBoardContent());	
 			return pstmt.executeUpdate();
 		}catch(Exception e){
 			e.printStackTrace();	
@@ -45,6 +63,15 @@ public class RBoardDAO {
 		return -1;
 	}
 	
-	
+	// 특정한 페이지에 있는 게시글들을 10개씩 보여주기
+	public ArrayList<BoardDTO> getList(int pageNumber){
+		String sql = "SELECT * FROM R_BOARD WHERE < ? ORDER BY BOARD_NO DESC LIMIT 10";
+		
+		
+		
+		
+		
+		return null;
+	}
 	
 }
